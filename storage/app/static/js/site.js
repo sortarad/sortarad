9b1856c167061b13fcf11ed4f89f69e2866b5eb4
Nick Hamze <me@nickhamze.com>
2021-01-11 15:19:11 -0600

    Manually add the assets to the static folder

---
+++ storage/app/static/js/site.js
@@ -0,0 +1,70 @@
+import interact from 'interactjs'
+import { createApp } from 'vue'
+
+import DesktopWindow from './components/DesktopWindow.vue';
+import VueClickAway from "vue3-click-away";
+
+const SortaApp = createApp({
+  data() {
+    return {
+      message: 'hi',
+      wallpaper: '/assets/system-wallpapers/leather-teal.png',
+      showingSettings: false
+    }
+  },
+  methods: {
+    setWallpaper(str) {
+      this.wallpaper = str;
+    },
+    showSettings() {
+      this.showingSettings = true;
+    },
+    generateSurprise() {
+      let surprises = [
+        'https://youtu.be/oHg5SJYRHA0',
+        'https://youtu.be/FGXDKrUoVrw',
+        'https://youtu.be/HcGNqrAtsgg',
+        'https://youtu.be/l_zsz_MlVvI',
+        'http://spacejam.com/'
+      ];
+
+      window.location = surprises[Math.floor(Math.random() * surprises.length)]
+    }
+  }
+})
+
+SortaApp.component('DesktopWindow', DesktopWindow)
+SortaApp.use(VueClickAway)
+SortaApp.mount('#app')
+
+
+
+
+
+/***********************************************/
+/* Draggable Interaction
+************************************************/
+
+interact('.window')
+  .draggable({
+    allowFrom: 'header',
+    listeners: { move: dragMoveListener,}
+  })
+
+interact('.shortcut')
+  .draggable({
+    listeners: { move: dragMoveListener,}
+  });
+
+function dragMoveListener (event) {
+  var target = event.target
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
