dd6b35b8727f13e1ee7320075afe7b6f25f74b5b
Jack McDade <jack@jackmcdade.com>
2021-02-24 08:18:28 -0500

    Finally figure out a way to manage window state with vue 3

--- resources/js/components/DesktopWindow.vue
+++ resources/js/components/DesktopWindow.vue
@@ -1,7 +1,7 @@
 <template>
     <div ref="window"
         class="mx-auto bg-tan font-display shadow border cursor-default window relative z-10"
-        :class="{width, position, 'hidden': ! isVisible}"
+        :class="{width, position}"
     >
         <header class="text-center py-1 flex items-center select-none">
             <div class="w-24"></div>
@@ -16,7 +16,7 @@
                 <div class="bg-black h-px mt-1"></div>
                 <div class="bg-black h-px mt-1"></div>
             </div>
-            <button class="w-24 outline-none relative" @click="close">
+            <button class="w-24 outline-none relative" @click="$emit('close')">
                 <svg xmlns="http://www.w3.org/2000/svg" height="13" width="13" viewBox="0 0 13 13" class="mx-auto"><line x1="1.41" y1="11.59" x2="11.59" y2="1.41" fill="none" stroke="currentColor" stroke-linecap="square" stroke-width="2"/><line x1="1.41" y1="1.41" x2="11.59" y2="11.59" fill="none" stroke="currentColor" stroke-linecap="square" stroke-width="2"/></svg>
             </button>
         </header>
@@ -30,16 +30,6 @@
 module.exports = {
     props: ['title', 'width', 'position'],
 
-    data: function() {
-        return {
-            isVisible: true
-        }
-    },
-
-    methods: {
-        close() {
-            this.isVisible = false
-        },
-    },
+    emits: ['close']
 }
 </script>
