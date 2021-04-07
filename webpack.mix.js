bfb1ed9087c6e27c500f9a425d1fa215130b57a2
Nick Hamze <me@nickhamze.com>
2021-04-07 15:24:45 -0500

    More reverts

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,13 +19,6 @@ mix.postCss('resources/css/tailwind.css', 'public/css', [
     require('postcss-preset-env')({stage: 0})
 ])
 
-mix.postCss('resources/css/site.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    require('postcss-preset-env')({stage: 0})
-])
-
 if (mix.inProduction()) {
    mix.version();
 }
