03044abcc1a66588d5ef22294d907c7c776bd2ef
Nick Hamze <me@nickhamze.com>
2021-05-15 21:26:14 -0500

    Create seo-pro.php

---
+++ config/statamic/seo-pro.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    'assets' => [
+        'container' => null,
+        'twitter_preset' => [
+            'w' => 1200,
+            'h' => 600,
+        ],
+        'open_graph_preset' => [
+            'w' => 1146,
+            'h' => 600,
+        ],
+    ],
+
+    'sitemap' => [
+        'enabled' => true,
+        'url' => 'sitemap.xml',
+        'expire' => 60,
+    ],
+
+    'humans' => [
+        'enabled' => true,
+        'url' => 'humans.txt',
+    ],
+
+    'pagination' => [
+        'enabled_in_canonical_url' => true,
+        'enabled_on_first_page' => false,
+    ],
+
+    'twitter' => [
+        'card' => 'summary_large_image',
+    ],
+
+    'alternate_locales' => true,
+
+];
